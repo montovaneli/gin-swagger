8290001098b2f5425acf6875130d7fadb76af59a
Douglas Montovaneli <montovaneli@poli.ufrj.br>
2025-06-03 16:59:28 +0100

    v1.7.2

--- example/gzipped/main.go
+++ example/gzipped/main.go
@@ -4,9 +4,9 @@ import (
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
-	"github.com/swaggo/gin-swagger"
+	"github.com/montovaneli/gin-swagger"
 
-	_ "github.com/swaggo/gin-swagger/example/basic/docs"
+	_ "github.com/montovaneli/gin-swagger/example/basic/docs"
 )
 
 // @title Swagger Example API
